1d1f1bf61415deaf6cab1e7671706b44699d3178
david <canino.david@gmail.com>
2016-07-29 14:21:01 +0200

    added the 'Chapter 3*' folder, containing other Python scripts (tests)

---
+++ Chapter 3 (Nozioni di Base)/script3.py
@@ -0,0 +1,30 @@
+a=1
+b=2
+c=-3
+d=4.0
+print()
+print("\tVariable 'a':",a)
+print("\tType of the variable 'a':",type(a),"\n")
+print("\tVariable 'b':",b)
+print("\tType of the variable 'b':",type(b),"\n")
+print("\tVariable 'c':",c)
+print("\tType of the variable 'c':",type(c),"\n")
+print("\tVariable 'd':",d)
+print("\tType of the variable 'd':",type(d),"\n")
+d=int(d)
+print("\tConversion of the variable 'd' as 'int':",d)
+print("\tType of the new variable 'd':",type(d),"\n")
+#ris_ab=a+b;
+#print("\tOperation a+b:",ris_ab,"with type",type(ris_ab))
+#ris_ab=a-b;
+#print("\tOperation a-b:",ris_ab,"with type",type(ris_ab))
+#ris_cd=c*d
+#print("\tOperation c*d:",ris_cd,"with type",type(ris_cd))
+#ris_cb=c/b
+#print("\tOperation c/b:",ris_cb,"with type",type(ris_cb))
+#ris_cb=c//b
+#print("\tOperation c//b:",ris_cb,"with type",type(ris_cb))
+#ris_db=d/b
+#print("\tOperation d/b:",ris_db,"with type",type(ris_db))
+#mod_db=d%b
+#print("\tOperation d%b:",mod_db,"with type",type(mod_db))
