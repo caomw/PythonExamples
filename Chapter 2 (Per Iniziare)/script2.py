caf05abdde09da3e730b0dda552e04ef82a3e7ff
david <canino.david@gmail.com>
2016-07-29 09:31:06 +0200

    added script2.py to the 'Chapter2*' folder

---
+++ Chapter 2 (Per Iniziare)/script2.py
@@ -0,0 +1,17 @@
+# First expression (with values in 'floating point' precision)
+print("Computing the expression \'2+10.0*5\' (with the values in 'floating point' precision):",2+10.0*5)
+
+# Second expression (with 'integer' values)
+print("Computing the expression \'2+10*5\' (with the integer values):",2+10*5)
+
+# Computing expression #1 (from the book)
+print("Computing the expression \'3.14+2.10*5.14\':",3.14+2.10*5.14)
+
+# Computing expression #2 (from the book)
+print("Computing the expression '2+5*6':",2+5*6)
+
+
+
+
+
+
