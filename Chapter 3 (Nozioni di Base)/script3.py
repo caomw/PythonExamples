94b9015038eb285f8e801aea9fe9e9fb67600b46
david <canino.david@gmail.com>
2016-07-30 18:21:11 +0200

    added a header to the 'script3.py' script in the 'Chapter 2*' folder, and other modifications to the same script

--- Chapter 3 (Nozioni di Base)/script3.py
+++ Chapter 3 (Nozioni di Base)/script3.py
@@ -1,3 +1,10 @@
+#################################################################################
+# David Canino, July 2016
+#
+# Examples about the conversion of some expressions in the Python Language
+#
+# script3.py
+#################################################################################
 a=1
 b=2
 c=-3
@@ -14,17 +21,17 @@ print("\tType of the variable 'd':",type(d),"\n")
 d=int(d)
 print("\tConversion of the variable 'd' as 'int':",d)
 print("\tType of the new variable 'd':",type(d),"\n")
-#ris_ab=a+b;
-#print("\tOperation a+b:",ris_ab,"with type",type(ris_ab))
-#ris_ab=a-b;
-#print("\tOperation a-b:",ris_ab,"with type",type(ris_ab))
-#ris_cd=c*d
-#print("\tOperation c*d:",ris_cd,"with type",type(ris_cd))
-#ris_cb=c/b
-#print("\tOperation c/b:",ris_cb,"with type",type(ris_cb))
-#ris_cb=c//b
-#print("\tOperation c//b:",ris_cb,"with type",type(ris_cb))
-#ris_db=d/b
-#print("\tOperation d/b:",ris_db,"with type",type(ris_db))
-#mod_db=d%b
-#print("\tOperation d%b:",mod_db,"with type",type(mod_db))
+ris_ab=a+b;
+print("\tOperation a+b:",ris_ab,"with type",type(ris_ab))
+ris_ab=a-b;
+print("\tOperation a-b:",ris_ab,"with type",type(ris_ab))
+ris_cd=c*d
+print("\tOperation c*d:",ris_cd,"with type",type(ris_cd))
+ris_cb=c/b
+print("\tOperation c/b:",ris_cb,"with type",type(ris_cb))
+ris_cb=int(c//b)
+print("\tOperation c//b:",ris_cb,"with type",type(ris_cb))
+ris_db=d/b
+print("\tOperation d/b:",ris_db,"with type",type(ris_db))
+mod_db=d%b
+print("\tOperation d%b:",mod_db,"with type",type(mod_db),"\n")
