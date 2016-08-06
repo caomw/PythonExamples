aaf681bdb7e3f612986d63a8260cd847571ad293
David Canino <canino.david@gmail.com>
2016-08-06 15:45:11 +0200

    added the 'Chapter4*' folder with new scripts

---
+++ Chapter 4 (Collezioni di Dati)/script6.py
@@ -0,0 +1,18 @@
+###########################################################################################
+#
+# David Canino, August 2016
+#
+# Basic management of the lists in the Python Language
+#
+# scritp6.py script
+###########################################################################################
+lst=[]
+print("\n\tList 'lst':",lst)
+print("\tType of the list 'lst':",type(lst))
+n=len(lst)
+print("\tThe number 'n' of the elements in the list 'lst':",n,"of type ",type(n),"\n")
+lst=[1,2,3,4]
+print("\tList 'lst':",lst)
+print("\tType of the list 'lst':",type(lst))
+n=len(lst)
+print("\tThe number 'n' of the elements in the list 'lst':",n,"of type ",type(n),"\n")
