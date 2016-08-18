5d70720fbb0424647badb0e5b9a2eeca1b0381b6
David Canino <canino.david@gmail.com>
2016-08-18 18:18:53 +0200

    modified the 'script17.py' and the 'script18.py' scripts in the 'Chapter6*' folder

--- Chapter 6 (Funzioni e Moduli)/script18.py
+++ Chapter 6 (Funzioni e Moduli)/script18.py
@@ -4,12 +4,12 @@
 #
 # Basic management of the custom modules functions in the Python Language
 #
-# scritp18.py script (the same as 'script14.py' script, but with the custom module)
+# scritp18.py script (the same as 'script14.py' script, but it exploits the custom module)
 ###########################################################################################
 
 from custom_module import *
 
-# The main component - it receives a positive integer value and computes its factorial!
+# The main component - it receives a positive integer value and computes its factorial by using the 'factorial' function from the custom module
 print()
 n=input("\tPlease, insert a positive integer value: ")
 n=int(n)
